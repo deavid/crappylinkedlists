2133449330453255c516c18346d17d3666021324
David Martinez Marti <deavidsedice@gmail.com>
2020-05-16 23:30:52 +0100

    include iterators

--- src/linked5/mod.rs
+++ src/linked5/mod.rs
@@ -20,9 +20,9 @@ copy, but not useful at all.
 
 So the only sane way is going with "next: Rc<RefCell<Node>>"
 */
-use std::rc::Rc;
 use std::cell::Ref;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 struct Node {
     value: i64,
@@ -36,22 +36,23 @@ struct List {
 }
 
 impl Node {
-    fn new(value: i64) -> Self {
+    // NOTE: These implementations are not used at all!
+    fn _new(value: i64) -> Self {
         Node {
             value,
             prev: None,
             next: None,
         }
-    }   
-    fn get_next(&self) -> Option<Ref<Node>> {
+    }
+    fn _get_next(&self) -> Option<Ref<Node>> {
         self.next.as_ref().map(|x| x.borrow())
     }
 
-    fn tail(rcnode: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
+    fn _tail(rcnode: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
         let rnode = rcnode.borrow();
         match &rnode.next {
             None => rcnode.clone(),
-            Some(next) => Node::tail(next.clone()),
+            Some(next) => Node::_tail(next.clone()),
         }
     }
 }
@@ -62,6 +63,18 @@ impl List {
             first: None,
             tail: None,
         }
+    }
+
+    fn from_vec(v: &[i64]) -> Self {
+        let mut l = Self::new();
+        for n in v {
+            l.append(*n);
+        }
+        l
+    }
+
+    fn to_vec(&self) -> Vec<i64> {
+        self.iter().collect()
     }
 
     fn concat(&mut self, other_list: List) {
@@ -76,23 +89,95 @@ impl List {
             muttail.next = Some(other);
         } else {
             self.tail = other_list.tail.clone();
-            self.first = Some(other); 
+            self.first = Some(other);
         }
     }
 
     fn append(&mut self, value: i64) {
-        let mut other = Node {value, next: None, prev: None};
+        let mut other = Node {
+            value,
+            next: None,
+            prev: None,
+        };
 
         if let Some(tail) = self.tail.clone() {
             let mut muttail = tail.borrow_mut();
             other.prev = Some(tail.clone());
             let otherref = Rc::new(RefCell::new(other));
-            self.tail = Some(otherref.clone()); 
+            self.tail = Some(otherref.clone());
             muttail.next = Some(otherref);
         } else {
             let otherref = Rc::new(RefCell::new(other));
-            self.first = Some(otherref.clone()); 
-            self.tail = Some(otherref); 
+            self.first = Some(otherref.clone());
+            self.tail = Some(otherref);
+        }
+    }
+
+    fn iter(&self) -> IterList {
+        IterList {
+            cursor: self.first.clone(),
+            revcursor: self.tail.clone(),
         }
     }
 }
+
+pub struct IterList {
+    cursor: Option<Rc<RefCell<Node>>>,
+    revcursor: Option<Rc<RefCell<Node>>>,
+}
+
+impl Iterator for IterList {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = self.cursor.as_ref().map(|c| c.borrow().value);
+
+        self.cursor = match self.cursor.as_ref() {
+            Some(node) => {
+                let reached_rcursor = if let Some(rnode) = self.revcursor.clone() {
+                    use std::ops::Deref;
+                    use std::ptr;
+                    ptr::eq(rnode.deref(), node.deref())
+                } else {
+                    false
+                };
+                if reached_rcursor {
+                    None
+                } else {
+                    let bnode = node.borrow();
+                    bnode.next.clone()
+                }
+            }
+            None => None,
+        };
+        ret
+    }
+}
+
+impl DoubleEndedIterator for IterList {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let ret = self.revcursor.as_ref().map(|c| c.borrow().value);
+        self.revcursor = match self.revcursor.as_ref() {
+            Some(node) => {
+                let reached_lcursor = if let Some(lnode) = self.cursor.clone() {
+                    use std::ops::Deref;
+                    use std::ptr;
+                    ptr::eq(lnode.deref(), node.deref())
+                } else {
+                    false
+                };
+                if reached_lcursor {
+                    None
+                } else {
+                    let bnode = node.borrow();
+                    bnode.prev.clone()
+                }
+            }
+            None => None,
+        };
+        ret
+    }
+}
+
+#[cfg(test)]
+mod test;
---
+++ src/linked5/test.rs
@@ -0,0 +1,51 @@
+use super::*;
+
+#[test]
+fn test_create() {
+    let want = vec![3, 4, 0, 1, 2, 5, 6, 7, 8];
+    let l = List::from_vec(&want);
+    let got = l.to_vec();
+    assert_eq!(want, got);
+}
+
+#[test]
+fn test_rev_iter() {
+    let v = vec![3, 4, 0, 1, 2, 5, 6, 7, 8];
+    let l = List::from_vec(&v);
+    let got: Vec<i64> = l.iter().rev().collect();
+    let want: Vec<i64> = v.iter().rev().cloned().collect();
+    assert_eq!(want, got);
+}
+
+#[test]
+fn test_concat() {
+    let data = vec![3, 8, 1, 2];
+    let mut test = data.clone();
+    let mut l = List::from_vec(&data);
+    for _ in 1..=10 {
+        l.concat(List::from_vec(&data));
+        test.extend(&data);
+    }
+    let lvec = l.to_vec();
+    assert_eq!(test, lvec);
+}
+
+#[test]
+fn test_concat_huge() {
+    // Testing performance here...
+    let data_prev = vec![
+        3, 8, 1, 2, 9, 5, 12, 6, 3, 1, 0, 7, 6, 5, 4, 3, 1, 6, 8, 9, 5, 3, 2, 1, 5, 7, 8, 4, 6,
+    ];
+    let mut data = data_prev.clone();
+    for _ in 1..10 {
+        data.extend(&data_prev);
+    }
+    let mut test = data.clone();
+    let mut l = List::from_vec(&data);
+    for _ in 1..=1000 {
+        l.concat(List::from_vec(&data));
+        test.extend(&data);
+    }
+    let lvec = l.to_vec();
+    assert_eq!(test, lvec);
+}
